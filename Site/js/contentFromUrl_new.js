7566ced3d1120d9411d9b38837c9e7785b0568ad
Realfsen <andreas.realfsen@gmail.com>
2020-10-15 12:52:50 +0200

    makes contentFromUrl less hardcoded

--- Site/js/contentFromUrl_new.js
+++ Site/js/contentFromUrl_new.js
@@ -2,10 +2,16 @@
 let content = window.location.hash
 content = content.substr(1, content.length)
 
-// console.log(window.location.hash)
+console.log(window.location.hash)
+
+let template = window.location.pathname
+template = template.substr(6, template.length)
+template = template.replace('.html', '')
+
+console.log(template)
 
 // Loads the article with the filename gotten from the URL and puts it in the element with the "article_container" id
-// JSONLoader('recipes/cake.json', 'templates/card.html', (content) => {
+// JSONLoader('recipes/'+content+'.json', 'templates/'+template+'.html, (content) => {
 // 	let container = document.getElementById('content_container')
 // 	container.innerHTML = content
 // })
