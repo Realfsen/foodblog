c2bea5d2c9f29fabe547bd56d375b01c3b07b118
Stian Gaustad <stiankg@stud.ntnu.no>
2020-10-25 22:39:09 +0100

    Added underline to the navigation bar via JS

--- Site/js/modularity.js
+++ Site/js/modularity.js
@@ -15,13 +15,14 @@
 
     // Create the centeredDiv element
     var centeredDiv = document.createElement("div");
-    centeredDiv.className = "centeredNavBarElements"
+    centeredDiv.className = "centeredNavBarElements";
 
     // Input the link to "Oppskrifter", appended to centeredDiv
     var RecipeButton = document.createElement("a");
     var RecipeButtonText = document.createTextNode("Oppskrifter");
     RecipeButton.className = "NavBarBtn";
     RecipeButton.href = "recipe_list.html";
+    RecipeButton.id = "NavBarRecipes";
     RecipeButton.appendChild(RecipeButtonText);
     centeredDiv.appendChild(RecipeButton);
 
@@ -30,6 +31,7 @@
     var TechniqueButtonText = document.createTextNode("Baketips");
     TechniqueButton.className = "NavBarBtn";
     TechniqueButton.href = "baking101.html";
+    TechniqueButton.id = "NavBarTechnique";
     TechniqueButton.appendChild(TechniqueButtonText);
     centeredDiv.appendChild(TechniqueButton);
 
@@ -38,6 +40,7 @@
     var ContactButtonText = document.createTextNode("Kontakt oss");
     ContactButton.className = "NavBarBtn";
     ContactButton.href = "contact.html";
+    ContactButton.id = "NavBarContact";
     ContactButton.appendChild(ContactButtonText);
     centeredDiv.appendChild(ContactButton);
     navBar.appendChild(centeredDiv);
@@ -76,4 +79,28 @@
     innerLink.appendChild(innerLinkText);
     innerDivR.appendChild(innerLink);
     foot.appendChild(innerDivR);
+
+    // Coloring active site in navigation bar.
+    var fullURL = window.location.href;
+    console.log(fullURL)
+    stringPositionStart = fullURL.lastIndexOf('/Site/')+6; // Finds the place in the URL (starting from the back) where "/Site" is and return the index. +6 means we start after the /Site/
+    stringPositionEnd = fullURL.lastIndexOf('.html'); // Find the ".html" in the URL 
+    console.log(stringPositionStart, stringPositionEnd)
+    var subPage = fullURL.slice(stringPositionStart, stringPositionEnd);
+    console.log(subPage)
+    var activeBarStyling = "0.2vw solid #4D3142";
+    // Check what page we are on and retrieve the correct element based on the URL
+    if (subPage.toLowerCase()==="contact"){
+        var currentElement = document.getElementById("NavBarContact");
+            console.log(currentElement)
+            currentElement.style.borderBottom = activeBarStyling;
+    } else if (subPage.toLowerCase()==="baking101") {
+            console.log(currentElement)
+            var currentElement = document.getElementById("NavBarTechnique");
+            currentElement.style.borderBottom = activeBarStyling;
+    } else if (subPage.toLowerCase()==="recipe_list"){
+            console.log(currentElement)
+            var currentElement = document.getElementById("NavBarRecipes");
+            currentElement.style.borderBottom = activeBarStyling;
+    } else {}
 })()
