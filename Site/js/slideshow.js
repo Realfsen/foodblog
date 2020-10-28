26c1f2be6bf30cc46d955fc3ad00656b0c7f031d
Realfsen <32299760+Realfsen@users.noreply.github.com>
2020-10-28 18:02:18 +0100

    Quickslideshow (#25)
    * adds slideshow
    
    * adds temporary slideshow

---
+++ Site/js/slideshow.js
@@ -0,0 +1,42 @@
+const bilder = ['img/slide_1.jpg', 'img/slide_2.jpg', 'img/slide_3.jpg']
+
+let imgTag = document.querySelector('#imgTag')
+let index = 0
+
+//Changes the picture by changing the source of the <img>-tag
+//Makes sure the index also always points to an image
+function slide() {
+	index = (index + 1) % bilder.length
+	imgTag.src = bilder[index]
+}
+
+//One slide forward
+function forward() {
+	clearInterval(inter_vall)
+	slide()
+}
+
+//One slide backwards
+function backward() {
+	clearInterval(inter_vall)
+	if (index == 0) {
+		index = bilder.length - 1
+	} else {
+		index--
+	}
+	imgTag.src = bilder[index]
+}
+
+//Saves images in an array so they won't be removed from RAM
+let images = []
+function preload(bilder) {
+	//Loops through the array and adds the images to the new array for preloading
+	for (let i = 0; i < bilder.length; i++) {
+		images[i] = new Image()
+		images[i].src = bilder[i]
+	}
+}
+
+//Runs the preload function and creates an interval varable, to easily create intervals in the future.
+preload(bilder)
+let inter_vall = setInterval(slide, 5000)
