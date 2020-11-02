0e255c18e12022e52b6e7085dbe11071bc828766
Realfsen <andreas.realfsen@gmail.com>
2020-11-02 18:36:57 +0100

    contentFromUrl hotfix

--- Site/js/contentFromUrl_new.js
+++ Site/js/contentFromUrl_new.js
@@ -1,24 +1,13 @@
 // Gets the hash value of the URL (the text behind the #) and removes the first character (#)
 let content = window.location.hash
-content = content.substr(1, content.length).toLowerCase()
+content = content.substr(1, content.length)
 
-console.log(content)
-
+// Splits content in type[1] and name[2]
 content = content.split('_')
-
-// let template = window.location.pathname
-// template = template.substr(6, template.length)
-// template = template.replace('.html', '')
-
-// console.log(template)
 
 // Loads the article with the filename gotten from the URL and puts it in the element with the "article_container" id
 JSONLoader(
-	content[0] +
-		's/' +
-		content[1].charAt(0).toUpperCase() +
-		content[1].slice(1) +
-		'.json',
+	content[0] + 's/' + content[1] + '.json',
 	'templates/' + content[0] + '.html',
 	(result) => {
 		let container = document.getElementById('content_container')
